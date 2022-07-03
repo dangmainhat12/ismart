75b1b428829cfd29c33c3d4f146fc44b2890f663
= <=>
2022-07-03 09:04:23 +0700

    add

--- resources/views/user/cart/complete.blade.php
+++ resources/views/user/cart/complete.blade.php
@@ -9,8 +9,8 @@
         <p>• Nhân viên cửa hàng sẽ liên hệ cho khách hàng trong thời gian sớm nhất để xác nhận đơn hàng của quý khách 1 lần
             nữa.</p>
         <p>• Nhân viên giao hàng sẽ liên hệ với quý khách qua Số Điện thoại trước khi giao hàng 24 tiếng.</p>
-        <p>• Địa chỉ cửa hàng: 785/8 Nguyễn Kiệm - Phường 3 - Quận Gò Vấp - TP HCM.</p>
-        <p>• Số điện thoại cửa hàng: 0232715212</p>
+        <p>• Địa chỉ cửa hàng: 235 Hoàng Quốc Việt - Bắc Từ Liêm - Hà Nội.</p>
+        <p>• Số điện thoại cửa hàng: 0987.654.321 - 0989.989.989</p>
         <p class="text-right return"><a class="btn btn-outline-primary" title="Quay lại trang chủ"
                 href="{{ route('user.index') }}">Quay lại trang chủ</a>
         </p>
